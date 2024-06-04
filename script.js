d741dab1411da3926a81ec21a232c4aa0ce82342
cuddiale <alessiocuddretto@hotmail.com>
2024-06-04 09:59:20 +0200

    Update

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+ (hundred|thousand|million|billion) dollars/i;
 
-const isSpam = (msg) => helpRegex.test(msg);
+const denyList = [helpRegex, dollarRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
